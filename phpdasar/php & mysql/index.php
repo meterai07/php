46aa95cc149bfd6f9f65322424b465497c226504
Tengku rafi <t.mrr39@gmail.com>
2022-12-26 13:30:43 +0700

    modified:   phpdasar/php & mysql/index.php

--- phpdasar/php & mysql/index.php
+++ phpdasar/php & mysql/index.php
@@ -20,6 +20,7 @@ if (isset($_POST["cari"])) {
 <html lang="en">
 
 <head>
+    <!-- test -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
