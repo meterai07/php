cae661af50325f4bdbfc996dc42165731929fdee
Tengku rafi <t.mrr39@gmail.com>
2023-01-03 22:28:33 +0700

    modified:   phpmvc/app/controllers/Home.php new file:   phpmvc/app/models/User_model.php

--- phpmvc/app/controllers/Home.php
+++ phpmvc/app/controllers/Home.php
@@ -2,8 +2,10 @@
     class Home extends Controller{
         public function index()
         {
+            $data['nama'] = $this->model('User_model')->getUser();
+
             $this->view('templates/header', ['judul' => 'Home']);
-            $this->view('home/index');
+            $this->view('home/index', $data);
             $this->view('templates/footer');
         }
     }
---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+    class User_model{
+        private $nama = 'Rafi';
+
+        public function getUser()
+        {
+            return $this->nama;
+        }
+    }
+?>
